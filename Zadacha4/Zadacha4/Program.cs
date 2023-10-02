4b8efb1fb502359452113c104275e61796f425be
EugeneTheBraixen <100828745+EugeneTheBraixen@users.noreply.github.com>
2023-10-02 12:20:59 +0300

    Add files via upload

---
+++ Zadacha4/Zadacha4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+// Интерфейс для рисуемых объектов
+interface IDrawable
+{
+    void Draw();
+}
+
+// Класс круга
+class Circle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисуем круг");
+    }
+}
+
+// Класс прямоугольника
+class Rectangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисуем прямоугольник");
+    }
+}
+
+// Класс треугольника
+class Triangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисуем треугольник");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив объектов, реализующих интерфейс IDrawable
+        IDrawable[] drawables = new IDrawable[]
+        {
+            new Circle(),
+            new Rectangle(),
+            new Triangle()
+        };
+
+        // Вызываем метод Draw() для каждого объекта из массива
+        foreach (var drawable in drawables)
+        {
+            drawable.Draw();
+        }
+
+        Console.ReadLine();
+    }
+}
